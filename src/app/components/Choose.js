d170b0e037435e9192e0e3bbcfa62e5314e15553
uzair-shakeel <uzairshakeel00000@gmail.com>
2024-08-02 00:50:24 +0500

    Choose section Added

--- src/app/components/Choose.js
+++ src/app/components/Choose.js
@@ -2,10 +2,10 @@
 export default function Choose() {
   return (
     <div className="bg-white py-12">
-      <div className="container mx-auto flex flex-col md:flex-row items-center space-y-6 md:space-y-0 md:space-x-12">
-        <div className="flex-1">
-          <img src="/main-car.png" alt="Main Car" className="max-w-full" />
-          <div className="flex space-x-4 mt-4">
+      <div className="container px-[8%] mx-auto flex flex-col md:flex-row justify-between items-center space-y-6 md:space-y-0 md:space-x-12">
+        <div className="flex-1 w-[40%]">
+          <img src="/Choose-img.png" alt="Main Car" className="max-w-full" />
+          {/* <div className="flex space-x-4 mt-4">
             <div className="border p-2 rounded">
               <img
                 src="/car-1.png"
@@ -48,9 +48,9 @@ export default function Choose() {
                 </div>
               </div>
             </div>
-          </div>
+          </div> */}
         </div>
-        <div className="flex-1 w-[35%]">
+        <div className=" w-[40%]">
           <h2 className="text-[16px] font-[700] leading-[25px] mb-2">
             WHY CHOOSE US
           </h2>
@@ -67,8 +67,10 @@ export default function Choose() {
                 />
               </div>
               <div>
-                <h3 className="text-lg font-semibold">Best price guaranteed</h3>
-                <p className="text-gray-600">
+                <h3 className="text-[14px] font-[600] leading-[21px]">
+                  Best price guaranteed
+                </h3>
+                <p className="text-[#6B6868] text-[11px] leading-[17px]">
                   Find a lower price? we'll refund you 100% of the difference
                 </p>
               </div>
@@ -77,9 +79,11 @@ export default function Choose() {
               <div className="bg-white p-1 flex items-center justify-center h-[40px] w-[40px] shadow-md rounded-lg mr-4">
                 <img src="/icons/AI.svg" alt="Best Price" className="w-auto " />
               </div>
-              <div>
-                <h3 className="text-lg font-semibold">Power of AI</h3>
-                <p className="text-gray-600">
+              <div className="w-[80%]">
+                <h3 className="text-[14px] font-[600] leading-[21px]">
+                  Power of AI
+                </h3>
+                <p className="text-[#6B6868] text-[11px] leading-[17px]">
                   Revolutionize your car rental experience with the cutting-edge
                   power of Artificial Intelligence
                 </p>
@@ -93,9 +97,11 @@ export default function Choose() {
                   className="w-auto "
                 />
               </div>
-              <div>
-                <h3 className="text-lg font-semibold">24-hour car delivery</h3>
-                <p className="text-gray-600">
+              <div className="w-[80%]">
+                <h3 className="text-[14px] font-[600] leading-[21px]">
+                  24-hour car delivery
+                </h3>
+                <p className="text-[#6B6868] text-[11px] leading-[17px]">
                   Book your car anytime and we will deliver it directly to you
                 </p>
               </div>
@@ -108,11 +114,11 @@ export default function Choose() {
                   className="w-auto "
                 />
               </div>
-              <div>
-                <h3 className="text-lg font-semibold">
+              <div className="w-[80%]">
+                <h3 className="text-[14px]  font-[600] leading-[21px]">
                   24/7 technical support
                 </h3>
-                <p className="text-gray-600">
+                <p className="text-[#6B6868] text-[11px] leading-[17px]">
                   Have you questions? Contact Carental support anytime when you
                   have a problem
                 </p>
