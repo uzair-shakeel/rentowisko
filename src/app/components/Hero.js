a661434d55f1dad8f1e6cc74fd5bde1b4ba65c3b
uzair-shakeel <uzairshakeel00000@gmail.com>
2024-08-07 16:36:24 +0500

    ....

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -124,22 +124,22 @@ export default function Hero() {
 
             {/* Mobile */}
             <div className="px-4 md:hidden block">
-              <div className="bg-white shadow-custom py-0.5 px-4 rounded-md flex items-center justify-between">
-                <div className="flex-1 flex items-start flex-col">
-                  <h3 className="text-[10px] font-extrabold leading-[15px] uppercase text-[#060C17]">
+              <div className=" shadow-custom py-0.5 px-4 rounded-md flex flex-col items-center justify-between">
+                <div className="flex-1 flex bg-white w-full rounded items-center py-3 gap-2 flex-col">
+                  <h3 className="text-[15px] font-extrabold leading-[18px] uppercase text-[#060C17]">
                     Location
                   </h3>
                   <input
                     type="text"
                     placeholder="Search your location.."
-                    className="border rounded border-none outline-none text-[10px]  text-[#5E5A5A] font-medium leading-[15px] bg-transparent"
+                    className="border rounded border-none outline-none text-[15px]  text-[#5E5A5A] font-medium leading-[15px] bg-transparent"
                   />
                 </div>
-                <div className="flex-1 flex flex-col my-2 border-l border-[#E8E4E4]">
-                  <h3 className="text-[10px] font-extrabold leading-[15px] text-[#060C17] pl-1">
+                <div className="flex-1 flex flex-col my-2 py-3 gap-2 bg-white w-full rounded items-center border-[#E8E4E4]">
+                  <h3 className="text-[15px] font-extrabold leading-[18px] text-[#060C17] pl-1">
                     Range in Km
                   </h3>
-                  <select className="bg-transparent text-[10px] leading-[15px] text-[#5E5A5A] font-medium outline-none custom-select">
+                  <select className="bg-transparent text-[15px] leading-[18px] text-[#5E5A5A] font-medium outline-none custom-select">
                     <option>Select</option>
                     <option>0-10</option>
                     <option>10-20</option>
@@ -149,33 +149,33 @@ export default function Hero() {
               </div>
 
               <div className="flex items-center justify-center gap-1.5 mt-2.5">
-                <div className="flex-1 flex flex-col bg-white p-2 rounded-md">
-                  <h3 className="text-[10px] font-extrabold leading-[15px] text-[#060C17] pl-1">
+                <div className="flex-1 flex flex-col items-center gap-1 bg-white p-2 rounded-md">
+                  <h3 className="text-[13px] font-extrabold leading-[15px] text-[#060C17] pl-1">
                     Type
                   </h3>
-                  <select className="bg-transparent pl-1 text-[10px] leading-[15px] text-[#5E5A5A] font-medium outline-none custom-select">
+                  <select className="bg-transparent pl-1 text-[13px] leading-[15px] text-[#5E5A5A] font-medium outline-none custom-select">
                     <option>Select</option>
                     <option>0-10</option>
                     <option>10-20</option>
                     <option>20-50</option>
                   </select>
                 </div>
-                <div className="flex-1 flex flex-col bg-white p-2 rounded-md">
-                  <h3 className="text-[10px] font-extrabold leading-[15px] text-[#060C17] pl-1">
+                <div className="flex-1 flex items-center gap-1 flex-col bg-white p-2 rounded-md">
+                  <h3 className="text-[13px] font-extrabold leading-[18px] text-[#060C17] pl-1">
                     Make
                   </h3>
-                  <select className="bg-transparent pl-1 text-[10px] leading-[15px] text-[#5E5A5A] font-medium outline-none custom-select">
+                  <select className="bg-transparent pl-1 text-[13px] leading-[18px] text-[#5E5A5A] font-medium outline-none custom-select">
                     <option>Select</option>
                     <option>0-10</option>
                     <option>10-20</option>
                     <option>20-50</option>
                   </select>
                 </div>
-                <div className="flex-1 flex flex-col bg-white p-2 rounded-md">
-                  <h3 className="text-[10px] font-extrabold leading-[15px] text-[#060C17] pl-1">
+                <div className="flex-1 flex flex-col items-center gap-1 bg-white p-2 rounded-md">
+                  <h3 className="text-[13px] font-extrabold leading-[18px] text-[#060C17] pl-1">
                     Model
                   </h3>
-                  <select className="bg-transparent pl-1 text-[10px] leading-[15px] text-[#5E5A5A] font-medium outline-none custom-select">
+                  <select className="bg-transparent pl-1 text-[13px] leading-[18px] text-[#5E5A5A] font-medium outline-none custom-select">
                     <option>Select Model</option>
                     <option>0-10</option>
                     <option>10-20</option>
@@ -185,7 +185,7 @@ export default function Hero() {
               </div>
 
               <div className="flex items-center justify-center w-full">
-                <button className="bg-[#629FFD] mt-2.5 w-[177px] h-[28px] rounded text-white text-[16px] font-bold leading-[16px]">
+                <button className="bg-[#629FFD] mt-2.5 py-4 w-full rounded text-white text-[16px] font-bold leading-[16px]">
                   Search
                 </button>
               </div>
